55e751b167af1c8ab721445434614a1f9a5a6450
John <rDr4g0n@gmail.com>
2014-09-27 23:34:25 -0500

    add basic script editor function

---
+++ js/ScriptEditor.js
@@ -0,0 +1,14 @@
+(function(){
+    "use strict";
+
+    function ScriptEditor(){
+     
+    }
+
+    ScriptEditor.prototype = {
+        constructor: ScriptEditor
+    
+    };
+
+    window.ScriptEditor = ScriptEditor;
+})();
